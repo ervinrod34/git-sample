f521788703553e8c2225d683680a4cf71bcef54c
ervin rodriguez <mtr746@my.utsa.edu>
2017-09-12 23:26:26 -0500

    Commit 2

--- src/sample/HelloGit.java
+++ src/sample/HelloGit.java
@@ -5,6 +5,9 @@ public class HelloGit {
 	public static void main(String[] args) {
 		
 		System.out.print("Hello Git!");
+		System.out.print("Goodbye Git!");
+		
+		System.err.println("I need to go to bed.");
 		
 	}
 	
